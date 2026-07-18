feb93788aa7003faa2a03ba39ecce7809e3ca5c4
r775962-pixel <r775962@gmail.com>
2026-07-18 23:19:49 +0300

    Fix: Add missing fields and constructor to CompilationSubResult

--- composite-builds/build-deps/jdk-compiler/src/main/java/openjdk/tools/sjavac/server/CompilationSubResult.java
+++ composite-builds/build-deps/jdk-compiler/src/main/java/openjdk/tools/sjavac/server/CompilationSubResult.java
@@ -1,15 +1,39 @@
 package openjdk.tools.sjavac.server;
 
+import openjdk.tools.javac.main.Main.Result;
+import java.net.URI;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
 public class CompilationSubResult {
-    public enum Result {
-        OK,
-        ERROR,
-        WARNING
+    public final Result result;
+    public final String stdout;
+    public final String stderr;
+    public final Map<String, Set<URI>> packageArtifacts;
+    public final Map<String, Map<String, Set<String>>> packageDependencies;
+    public final Map<String, Map<String, Set<String>>> packageCpDependencies;
+    public final Map<String, Object> packagePubapis;
+    public final Map<String, Object> dependencyPubapis;
+
+    public CompilationSubResult(Result result) {
+        this(result, "", "", new HashMap<>(), new HashMap<>(), new HashMap<>(), new HashMap<>(), new HashMap<>());
     }
 
-    public final Result status;
-
-    public CompilationSubResult(Result status) {
-        this.status = status;
+    public CompilationSubResult(Result result, String stdout, String stderr,
+                                Map<String, Set<URI>> packageArtifacts,
+                                Map<String, Map<String, Set<String>>> packageDependencies,
+                                Map<String, Map<String, Set<String>>> packageCpDependencies,
+                                Map<String, Object> packagePubapis,
+                                Map<String, Object> dependencyPubapis) {
+        this.result = result;
+        this.stdout = stdout;
+        this.stderr = stderr;
+        this.packageArtifacts = packageArtifacts;
+        this.packageDependencies = packageDependencies;
+        this.packageCpDependencies = packageCpDependencies;
+        this.packagePubapis = packagePubapis;
+        this.dependencyPubapis = dependencyPubapis;
     }
 }
