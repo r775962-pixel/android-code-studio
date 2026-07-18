dc78a42cb58a515269831d8bebd9f33cfef93246
r775962-pixel <r775962@gmail.com>
2026-07-18 23:03:24 +0300

    Add missing ServerMain class

---
+++ composite-builds/build-deps/jdk-compiler/src/main/java/openjdk/tools/sjavac/server/ServerMain.java
@@ -0,0 +1,9 @@
+package openjdk.tools.sjavac.server;
+
+public class ServerMain {
+    public static int run(String[] args) {
+        // Server mode implementation
+        System.out.println("Server mode not fully implemented");
+        return 0;
+    }
+}
