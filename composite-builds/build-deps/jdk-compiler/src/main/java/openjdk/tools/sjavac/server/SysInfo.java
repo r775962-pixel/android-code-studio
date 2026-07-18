40a1bfd03d580fd0cdc0b97347add3488e0d1d9e
r775962-pixel <r775962@gmail.com>
2026-07-18 23:19:58 +0300

    Fix: Rename totalMemory to maxMemory to match usage in CompileJavaPackages

--- composite-builds/build-deps/jdk-compiler/src/main/java/openjdk/tools/sjavac/server/SysInfo.java
+++ composite-builds/build-deps/jdk-compiler/src/main/java/openjdk/tools/sjavac/server/SysInfo.java
@@ -2,10 +2,10 @@ package openjdk.tools.sjavac.server;
 
 public class SysInfo {
     public final int numCores;
-    public final long totalMemory;
+    public final long maxMemory;
 
-    public SysInfo(int numCores, long totalMemory) {
+    public SysInfo(int numCores, long maxMemory) {
         this.numCores = numCores;
-        this.totalMemory = totalMemory;
+        this.maxMemory = maxMemory;
     }
 }
